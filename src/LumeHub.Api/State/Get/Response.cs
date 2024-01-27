9f666532fa820888827852f2557d78b48e1f1f40
Jesse Gollub <jesse.gollub@ewe.net>
2024-01-27 13:30:22 +0100

    added get and set state endpoints

---
+++ src/LumeHub.Api/State/Get/Response.cs
@@ -0,0 +1,6 @@
+﻿namespace LumeHub.Api.State.Get;
+
+public sealed class Response
+{
+    public required bool IsOn { get; init; }
+}
---
+++ src/LumeHub.Api/State/Set/Endpoint.cs
@@ -0,0 +1,15 @@
+﻿using LumeHub.Api.Effects;
+
+namespace LumeHub.Api.State.Set;
+
+public sealed class Endpoint(IManager manager) : Endpoint<Request>
+{
+    public override void Configure() => Post("state");
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+        manager.Toggle(req.State);
+        Logger.LogInformation("Set state of the led strip to {State}.", req.State);
+        await SendNoContentAsync(ct);
+    }
+}
