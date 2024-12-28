5fe038a5cd114d4d0d41fc75004009304f0ae246
anders130 <93037023+anders130@users.noreply.github.com>
2024-12-28 17:02:52 +0100

    switch to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,36 +1,38 @@
 {
-    description = "Development Environment Flake";
+    description = "LumeHub.Server flake";
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-        flake-utils.url = "github:numtide/flake-utils";
+        flake-parts.url = "github:hercules-ci/flake-parts";
     };
 
-    outputs = { flake-utils, nixpkgs, self, ... }:
-        flake-utils.lib.eachDefaultSystem (system: let
-            pkgs = import nixpkgs {inherit system;};
-            dotnet-sdk = pkgs.dotnet-sdk_8;
-            aspnetcore = pkgs.dotnet-aspnetcore_8;
-        in {
-            devShells.default = pkgs.mkShell {
-                packages = [dotnet-sdk];
-            };
+    outputs = inputs:
+        inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+            systems = [
+                "x86_64-linux"
+                "aarch64-linux"
+            ];
+            flake.nixosModules.default = import ./nix/nixosModule.nix inputs.self;
+            perSystem = {pkgs, ...}: let
+                dotnet-sdk = pkgs.dotnet-sdk_8;
+                aspnetcore = pkgs.dotnet-aspnetcore_8;
+            in {
+                devShells.default = pkgs.mkShell {
+                    packages = [dotnet-sdk];
+                };
+                packages.default = pkgs.buildDotnetModule {
+                    inherit dotnet-sdk;
+                    name = "LumeHub.Server";
+                    version = "0.1.0";
+                    src = ./.;
+                    projectFileName = "src/LumeHub.Server/LumeHub.Server.csproj";
+                    nugetDeps = ./nix/deps.nix;
+                    frameworkDeps = [aspnetcore];
 
-            packages.default = pkgs.buildDotnetModule {
-                inherit dotnet-sdk;
-                name = "LumeHub.Server";
-                version = "0.1.0";
-                src = ./.;
-                projectFileName = "src/LumeHub.Server/LumeHub.Server.csproj";
-                nugetDeps = ./nix/deps.nix;
-                frameworkDeps = [aspnetcore];
-
-                makeWrapperArgs = [
-                    "--set DOTNET_ROOT ${aspnetcore}"
-                ];
+                    makeWrapperArgs = [
+                        "--set DOTNET_ROOT ${aspnetcore}"
+                    ];
+                };
             };
-        })
-        // {
-            nixosModules.default = import ./nix/nixosModule.nix self;
         };
 }
