9b5e6b85e5706c7bbd0bb02a111a673fd771db61
anders130 <93037023+anders130@users.noreply.github.com>
2026-03-12 09:18:46 +0100

    feat: add Zone to domain crate

--- crates/domain/src/lib.rs
+++ crates/domain/src/lib.rs
@@ -1,4 +1,6 @@
 pub mod blend_mode;
 pub mod color;
+pub mod zone;
 pub use blend_mode::BlendMode;
 pub use color::Rgb;
+pub use zone::Zone;
---
+++ crates/domain/src/zone.rs
@@ -0,0 +1,6 @@
+pub struct Zone {
+    pub id: u32,
+    pub name: String,
+    pub start: usize,
+    pub end: usize,
+}
