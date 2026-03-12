e13c2d28af4fbcfe6d0b0f9e9552c7af8f9b9b8f
anders130 <93037023+anders130@users.noreply.github.com>
2026-03-12 09:21:55 +0100

    feat: add DeviceState to domain crate

---
+++ crates/domain/src/device_state.rs
@@ -0,0 +1,7 @@
+use crate::Rgb;
+
+pub struct DeviceState {
+    pub on: bool,
+    pub brightness: u8,
+    pub color: Rgb,
+}
--- crates/domain/src/lib.rs
+++ crates/domain/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod blend_mode;
 pub mod color;
+pub mod device_state;
 pub mod zone;
 pub use blend_mode::BlendMode;
 pub use color::Rgb;
+pub use device_state::DeviceState;
 pub use zone::Zone;
